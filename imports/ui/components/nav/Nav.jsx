00990d1ec3d82eac71e2325ea0ff7d286ee148bb
Ivan Baena <ivan.baena04@gmail.com>
2016-12-27 17:21:36 -0500

    leaving work

--- imports/ui/components/nav/Nav.jsx
+++ imports/ui/components/nav/Nav.jsx
@@ -3,12 +3,21 @@ import React, { Component } from 'react';
 
 // App component - represents the whole app
 export default class Nav extends Component {
+  // Note: In ES6, constructor() === componentWillMount() in React ES5
+  constructor() {
+    super();
 
+  }
+
+  componentDidMount(){
+
+  }
 
   render() {
     return (
       <nav className="nav-container">
-          Nav
+          <div className="blue-bg"></div>
+          <div className="red-bg"></div>
       </nav>
     );
   }
