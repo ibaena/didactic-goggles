4d48ad0d4de1e109c14095ccebd7be0fa6fe4249
ibaena <ivan.baena04@gmail.com>
2016-12-28 00:26:13 -0500

    ZZzzZZzz

---
+++ imports/ui/components/landingRight/LandingRight.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+// App component - represents the whole app
+export default class LandingRight extends Component {
+  // Note: In ES6, constructor() === componentWillMount() in React ES5
+  constructor() {
+    super();
+
+  }
+
+  componentDidMount(){
+
+  }
+  render() {
+    return (
+      <div id="landing-r-container">
+
+      </div>
+    );
+  }
+}
--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
-
+import LandingLeft from '../../ui/components/landingLeft/LandingLeft.jsx';
+import LandingRight from '../../ui/components/landingRight/LandingRight.jsx';
 
 // App component - represents the whole app
 export const Home = () =>
   <div id="homePage">
-
+    <LandingLeft />
+    <LandingRight />
   </div>
