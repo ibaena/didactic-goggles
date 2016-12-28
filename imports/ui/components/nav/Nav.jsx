a72420e5c93a134fcfb2c3388e5fcfd31fc877a7
ibaena <ivan.baena04@gmail.com>
2016-12-27 23:55:50 -0500

    nav btn added

--- imports/ui/components/nav/Nav.jsx
+++ imports/ui/components/nav/Nav.jsx
@@ -10,14 +10,29 @@ export default class Nav extends Component {
   }
 
   componentDidMount(){
-
+    $('.menu').hide();
+    $('.activate-menu').on('click', () => {
+      $('.menu').toggle("slide", { direction: "right" }, 500)
+    });
+    $('.btn--menu').on('click', () => {
+        $('.not--x__piece--1, .not--x__piece--2, .not--x__piece--3, .not--x__piece--4').toggleClass('is--x');
+      });
   }
-
   render() {
     return (
       <nav className="nav-container">
-          <div className="blue-bg"></div>
-          <div className="red-bg"></div>
+        <button type='button' className='btn btn--menu activate-menu'>
+          <span className='x__piece'></span>
+          <span className='not--x__piece not--x__piece--1'></span>
+          <span className='x__piece'></span>
+          <span className='not--x__piece not--x__piece--2'></span>
+          <span className='x__piece'></span>
+          <span className='not--x__piece not--x__piece--3'></span>
+          <span className='x__piece'></span>
+          <span className='not--x__piece not--x__piece--4'></span>
+          <span className='x__piece'></span>
+        </button>
+          <div className="menu"></div>
       </nav>
     );
   }
--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -5,5 +5,5 @@ import React, { Component } from 'react';
 // App component - represents the whole app
 export const Home = () =>
   <div id="homePage">
-    Homepage
+
   </div>
